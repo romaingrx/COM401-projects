0b8db6a77a9d421c3330bb0ea75d5b713bc4bfda
Romain Graux <romain.graux@student.uclouvain.be>
2021-10-30 12:56:21 +0200

    Added parameters and file parser

---
+++ hw2/utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author : Romain Graux
+@date : 2021 Oct 30, 12:28:36
+@last modified : 2021 Oct 30, 12:55:43
+"""
+
+import re, sys
+
+
+def get_value_from_parameters(filename: str, key: str):
+    with open(filename, "r") as fd:
+        raw_text = fd.read()
+    z = re.findall(f"^[\s]*{key}[\s]*=[\s]*(.+)[\s]*$", raw_text, re.MULTILINE)
+    if len(z) != 1:
+        if len(z):
+            print("Found several matches for key `{key}` : ", z, file=sys.stderr)
+        return None
+    return z[0].replace("'", '"')  # TODO: replace?
+
+
+if __name__ == "__main__":
+    import json
+
+    value = get_value_from_parameters("345081-parameters.txt", "Q3_dict")
+    print(json.loads(value))
