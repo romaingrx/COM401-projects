325e2d4d79faddf0e3e1cfa89bf6c53e4738a2ac
Romain Graux <romain.graux@student.uclouvain.be>
2021-12-09 14:59:39 +0100

    Solved Ex2 + cleanup

--- hw3/ex2.py
+++ hw3/ex2.py
@@ -3,7 +3,7 @@
 """
 @author : Romain Graux
 @date : 2021 Dec 07, 11:40:12
-@last modified : 2021 Dec 07, 16:31:27
+@last modified : 2021 Dec 09, 14:39:22
 """
 
 # RSA LSB Oracle attack
@@ -12,9 +12,13 @@
 # Paillier LSB Oracle attack
 # https://gist.github.com/73spica/0ff38748cdc07747857b4688ea5f1f6a
 
-from utils import get_values_from_parameters, ask_oracle
-from sage.all import *
-import socket, subprocess
+from utils import get_values_from_parameters
+from extended_euclidian_algorithm import extended_euclidian_algorithm
+
+import os, random
+from sage.all import power_mod
+from fractions import Fraction
+from math import floor, ceil, log2
 
 SCIPER = 345081
 PARAMETERS_FILENAME = f"{SCIPER}-parameters.txt"
@@ -22,57 +26,54 @@ PARAMETERS_FILENAME = f"{SCIPER}-parameters.txt"
 (n, g), c = get_values_from_parameters(PARAMETERS_FILENAME, ("Q2_pk", "Q2_c"))
 
 
-def get_lsb(value):
-    return int(
-        subprocess.check_output(
-            [
-                "echo",
-                str(SCIPER),
-                str(value),
-                "|",
-                "ncat",
-                "lasicpi1.epfl.ch",
-                "5555",
-            ],
-            shell=True,
-        )
-    )
+LSB = lambda value: int(
+    os.popen(f"echo {SCIPER} {value} | nc lasecpi1.epfl.ch 5555").read()
+)
 
 
-def get_lsb(value):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sockfd:
-        sockfd.settimeout(2)
-        sockfd.connect(("lasecpi1.epfl.ch", 5555))
-        sockfd.send(f"{SCIPER} {value}\n".encode())
-        recv_value = sockfd.recv(1).decode()
-    return int(recv_value)
+def attack_paillier_lsb(c, decrypt_lsb, n, steps=None):
+    bounds = [0, Fraction(n)]
+
+    steps = steps or ceil(log2(n))
+    for step in range(1, steps + 1):
+        c = (c * c) % (n * n)
+        lsb = decrypt_lsb(c)
+
+        # if we have a bit for lsb, then set the lower bound as lb+ub/2, otherwise set it for the upper bound
+        bounds[1 - lsb] = sum(bounds) / 2
+        upper_bound, lower_bound = floor(bounds[1]), ceil(bounds[0])
+        if lower_bound == upper_bound:
+            print()
+            return lower_bound
+
+        print(f"Step {step}/{steps}", end="\r")
+
+    print("\nThe message was not found")
+    return bounds
 
 
-lb, ub = Integer(0), Integer(n)
-cipher = c
-i = 0
-while True:
-    try:
-        lsb = get_lsb((cipher * cipher) % (n * n))
-    except socket.timeout:
-        input("Type any key to continue")
-        continue
-    except socket.gaierror:
-        input("Type any key to continue")
-        continue
-    if lsb:
-        lb = (lb + ub) / 2
-    else:
-        ub = (lb + ub) / 2
-    diff = ub - lb
-    if diff.numerator() / diff.denominator() == 0:
-        m = ub
-        break
-    i += 1
-    print(
-        f"Current step : {i}",
-        end="\r",
-    )
-    cipher = (cipher * cipher) % (n * n)
-print(i)
-print(m)
+UNIT_TEST = True
+if UNIT_TEST:
+
+    def random_unit_element(n):
+        a = random.randint(1, n)
+        gcd, u, v = extended_euclidian_algorithm(a, n)
+        if gcd != 1:
+            return random_unit_element(n)
+        return a
+
+    # r = random_unit_element(n)  # The random value in Z_n^*
+    r = 1
+    message = 42  # The message
+    c = (g ** message) * (r ** n) % (n * n)
+    # c = (
+    #     power_mod(g, message, n * n) * power_mod(r, n, n * n) % (n * n)
+    # )  # Encrypt message
+    decrypted_message = attack_paillier_lsb(c, LSB, n, steps=4096)
+    print(decrypted_message)
+    assert (
+        decrypted_message == message
+    ), f"decrypted message {decrypted_message} != message {message}"
+else:
+    m = attack_paillier_lsb(c, LSB, n)
+    print(f"Q2_m={m}")
--- hw3/utils.py
+++ hw3/utils.py
@@ -3,10 +3,10 @@
 """
 @author : Romain Graux (345081)
 @date : 2021 Oct 30, 12:21:25
-@last modified : 2021 Dec 07, 14:15:28
+@last modified : 2021 Dec 09, 14:58:10
 """
 
-import re, sys, ast, socket
+import re, sys, ast
 
 
 def get_values_from_parameters(filename: str, keys: str):
@@ -28,22 +28,3 @@ def get_values_from_parameters(filename: str, keys: str):
         raise TypeError(
             f"type of keys not understood : {type(keys)}, only accept str, list or tuple"
         )
-
-
-def ask_oracle(sciper: int, port: int, value, hostname: str = "lasecpi1.epfl.ch"):
-    def send_recv(sockfd, v):
-        pkt = f"{sciper} {v}".encode()
-        sockfd.send(pkt)
-        return int(sockfd.recv(1).decode())
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sockfd:
-        sockfd.connect((hostname, port))
-        if hasattr(value, "__iter__"):
-            return [send_recv(sockfd, v) for v in value]
-        else:
-            return send_recv(sockfd, v)
-
-
-if __name__ == "__main__":
-    b = ask_oracle(345081, 5555, 1)
-    print(b)
